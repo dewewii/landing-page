385079bd8568ab1be0b12d82e39b6dca74a4a771
dewewii <120241308+dewewii@users.noreply.github.com>
2025-04-23 15:33:39 +0700

    produk.js

--- produk.js
+++ produk.js
@@ -1,17 +1,21 @@
+// produkscroll
 document.addEventListener("DOMContentLoaded", function () {
   const scrollContainer = document.getElementById("produkScroll");
+  const leftButton = document.querySelector(".button-arrow-left");
+  const rightButton = document.querySelector(".button-arrow-right");
 
-  window.scrollLeft = function () {
+  leftButton.addEventListener("click", function () {
     scrollContainer.scrollBy({
       left: -300,
       behavior: "smooth",
     });
-  };
+  });
 
-  window.scrollRight = function () {
+  rightButton.addEventListener("click", function () {
     scrollContainer.scrollBy({
       left: 300,
       behavior: "smooth",
     });
-  };
+  });
 });
+// end produkscroll
